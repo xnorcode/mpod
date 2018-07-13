4171b6b7f928cb9e68097612d83e01b53c135c38
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-13 13:04:57 +0300

    refactor: renamed list activity

--- app/src/main/java/com/mpod/ui/list/ListActivity.java
+++ app/src/main/java/com/mpod/ui/list/ListActivity.java
@@ -1,13 +1,15 @@
-package com.mpod;
+package com.mpod.ui.list;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+import com.mpod.R;
+
+public class ListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_list);
     }
 }
