004ef5f5b762b48b6d2e603d1d5b0eff23e257dc
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-15 14:33:41 +0300

    refactor: created search activity contract

---
+++ app/src/main/java/com/mpod/ui/search/SearchContract.java
@@ -0,0 +1,38 @@
+package com.mpod.ui.search;
+
+import com.mpod.ui.base.BasePresenter;
+import com.mpod.ui.base.BaseView;
+
+/**
+ * Created by xnorcode on 15/07/2018.
+ */
+public interface SearchContract {
+
+    interface View extends BaseView<Presenter> {
+
+
+        /**
+         * proceed to list activity
+         */
+        void startListActivity();
+
+
+        /**
+         * Show error
+         *
+         * @param msg The error message to be shown
+         */
+        void showError(String msg);
+    }
+
+    interface Presenter extends BasePresenter<View> {
+
+
+        /**
+         * start search repositories for the given artist
+         *
+         * @param artistName The artist name to search for
+         */
+        void startSearch(String artistName);
+    }
+}
