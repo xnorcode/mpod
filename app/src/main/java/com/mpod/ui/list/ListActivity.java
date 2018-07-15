a031b68e11d22baec834db7b88a83ff46f713b1b
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-15 22:43:33 +0300

    refactor: forwarding intent data to fragment

--- app/src/main/java/com/mpod/ui/list/ListActivity.java
+++ app/src/main/java/com/mpod/ui/list/ListActivity.java
@@ -1,8 +1,10 @@
 package com.mpod.ui.list;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.mpod.R;
+import com.mpod.utils.AppConstants;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
@@ -21,6 +23,15 @@ public class ListActivity extends DaggerAppCompatActivity {
         ListFragment fragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.list_fragment_container);
         if (fragment == null) {
             fragment = new ListFragment();
+
+            // get extra from intent and add in fragment
+            Intent intent = getIntent();
+            if (intent != null && intent.hasExtra(AppConstants.ARTIST_NAME)) {
+                Bundle args = new Bundle();
+                args.putString(AppConstants.ARTIST_NAME, intent.getStringExtra(AppConstants.ARTIST_NAME));
+                fragment.setArguments(args);
+            }
+
             getSupportFragmentManager().beginTransaction().add(R.id.list_fragment_container, fragment).commit();
         }
     }
