15ff7f0c4f6abd46b54d7ff036583b8914378086
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-15 12:09:14 +0300

    refactor: created splash activity contract

---
+++ app/src/main/java/com/mpod/ui/splash/SplashActivityContract.java
@@ -0,0 +1,28 @@
+package com.mpod.ui.splash;
+
+import com.mpod.ui.base.BasePresenter;
+import com.mpod.ui.base.BaseView;
+
+/**
+ * Created by xnorcode on 15/07/2018.
+ */
+public interface SplashActivityContract {
+
+    interface View extends BaseView<Presenter> {
+
+
+        /**
+         * start next activity
+         */
+        void startNextActivity();
+    }
+
+    interface Presenter extends BasePresenter<View> {
+
+
+        /**
+         * proceed to home activity
+         */
+        void proceed();
+    }
+}
