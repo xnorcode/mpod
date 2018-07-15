3d78a099f06750dc131163bf02296d3f75a43b37
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-15 15:45:17 +0300

    refactor: created search activity

--- app/src/main/java/com/mpod/ui/search/SearchActivity.java
+++ app/src/main/java/com/mpod/ui/search/SearchActivity.java
@@ -1,9 +1,34 @@
 package com.mpod.ui.search;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.mpod.R;
+
+import dagger.android.support.DaggerAppCompatActivity;
 
 /**
  * Created by xnorcode on 13/07/2018.
  */
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends DaggerAppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_search);
+
+        // remove activity's background theme
+        getWindow().setBackgroundDrawable(null);
+
+        // add fragment to activity
+        SearchFragment fragment = (SearchFragment) getSupportFragmentManager().findFragmentById(R.id.search_fragment_container);
+        if (fragment == null) {
+            fragment = new SearchFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.search_fragment_container, fragment).commit();
+        }
+    }
+
+
 }
