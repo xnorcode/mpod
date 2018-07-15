6fa5330623cad9aee93636142e0b22dc64c992ac
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-16 00:41:33 +0300

    refactor: added repository getter method in application for tests

--- app/src/main/java/com/mpod/MpodApplication.java
+++ app/src/main/java/com/mpod/MpodApplication.java
@@ -1,6 +1,11 @@
 package com.mpod;
 
+import android.support.annotation.VisibleForTesting;
+
+import com.mpod.data.source.ArtistRepository;
 import com.mpod.di.DaggerAppComponent;
+
+import javax.inject.Inject;
 
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
@@ -10,8 +15,22 @@ import dagger.android.DaggerApplication;
  */
 public class MpodApplication extends DaggerApplication {
 
+    @Inject
+    ArtistRepository mArtistRepository;
+
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
         return DaggerAppComponent.builder().application(this).build();
+    }
+
+
+    /**
+     * This is used only in tests to delete all artists from local db
+     *
+     * @return
+     */
+    @VisibleForTesting
+    public ArtistRepository getArtistRepository() {
+        return mArtistRepository;
     }
 }
