d671b18c8c565e0efbb67d16fa4d7d71f7ac0e8d
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-15 13:45:22 +0300

    refactor: created splash module for dagger injection

--- app/src/main/java/com/mpod/di/AppComponent.java
+++ app/src/main/java/com/mpod/di/AppComponent.java
@@ -4,13 +4,14 @@ import android.app.Application;
 
 import com.mpod.MpodApplication;
 import com.mpod.data.source.ArtistRepositoryModule;
+import com.mpod.ui.splash.SplashModule;
 
 import javax.inject.Singleton;
 
 import dagger.BindsInstance;
 import dagger.Component;
-import dagger.android.AndroidInjectionModule;
 import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
 
 /**
  * Created by xnorcode on 13/07/2018.
@@ -18,7 +19,8 @@ import dagger.android.AndroidInjector;
 @Singleton
 @Component(modules = {ArtistRepositoryModule.class,
         ApplicationModule.class,
-        AndroidInjectionModule.class})
+        SplashModule.class,
+        AndroidSupportInjectionModule.class})
 public interface AppComponent extends AndroidInjector<MpodApplication> {
 
     @Component.Builder
