e1f57e51ac44857cb2c44b78d84f20714fb45ee8
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-15 23:43:25 +0300

    refactor: testing search screen

--- app/src/androidTest/java/com/mpod/ui/search/SearchScreenTest.java
+++ app/src/androidTest/java/com/mpod/ui/search/SearchScreenTest.java
@@ -3,6 +3,7 @@ package com.mpod.ui.search;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.RootMatchers;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
@@ -10,6 +11,7 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.mpod.R;
 
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,14 +33,34 @@ public class SearchScreenTest {
 
         // verify search box is displayed
         Espresso.onView(ViewMatchers.withId(R.id.search_box))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+                // check hint is correct
+                .check(ViewAssertions.matches(ViewMatchers.withHint(R.string.hint_text)));
+
+        // type and search for artist
+        Espresso.onView(ViewMatchers.withId(R.id.search_box))
+                .perform(ViewActions.replaceText("Cher"), ViewActions.pressImeActionButton());
+
+        // verify that list activity is shown
+        Espresso.onView(ViewMatchers.withId(R.id.list_fragment_container))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+    @Test
+    public void searchArtist_SearchBoxInvalidInput() {
+
+        // verify search box is displayed
+        Espresso.onView(ViewMatchers.withId(R.id.search_box))
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
 
-        // check hint correct
+        // type empty in the search box
         Espresso.onView(ViewMatchers.withId(R.id.search_box))
-                // check hint is correct
-                .check(ViewAssertions.matches(ViewMatchers.withHint(R.string.hint_text)))
-                // type and search for artist
-                .perform(ViewActions.replaceText("Cher"), ViewActions.pressImeActionButton());
+                .perform(ViewActions.replaceText(""), ViewActions.pressImeActionButton());
+
+        // verify toast message is displayed
+        Espresso.onView(ViewMatchers.withText("Invalid input please try again..."))
+                .inRoot(RootMatchers.withDecorView(Matchers.not(mSearchActivityTestRule.getActivity().getWindow().getDecorView())))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 
 }
