3e827a62411c156cbcc517a0846fccaa94f8042f
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-15 13:42:01 +0300

    refactor: created splash activity

--- app/src/main/java/com/mpod/ui/splash/SplashActivity.java
+++ app/src/main/java/com/mpod/ui/splash/SplashActivity.java
@@ -1,9 +1,49 @@
 package com.mpod.ui.splash;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+
+import com.mpod.ui.search.SearchActivity;
+
+import javax.inject.Inject;
+
+import dagger.android.support.DaggerAppCompatActivity;
 
 /**
  * Created by xnorcode on 13/07/2018.
  */
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends DaggerAppCompatActivity implements SplashContract.View {
+
+
+    @Inject
+    SplashContract.Presenter mPresenter;
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // set view
+        mPresenter.setView(this);
+
+        // we usually check internet connection or perform
+        // any other tasks here before proceeding into the app
+        mPresenter.proceed();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPresenter.dropView();
+    }
+
+    @Override
+    public void startNextActivity() {
+        startActivity(new Intent(this, SearchActivity.class));
+        finish();
+    }
+
+    @Override
+    public void setPresenter(SplashContract.Presenter presenter) {
+        // used for manual injection of the presenter
+    }
 }
