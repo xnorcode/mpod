f2d11ee8d18f2d2048954f11b31928a7d62b6ce7
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-15 15:53:42 +0300

    refactor: created search presenter

---
+++ app/src/main/java/com/mpod/ui/search/SearchPresenter.java
@@ -0,0 +1,53 @@
+package com.mpod.ui.search;
+
+import com.mpod.data.source.ArtistRepository;
+
+import javax.inject.Inject;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by xnorcode on 15/07/2018.
+ */
+public class SearchPresenter implements SearchContract.Presenter {
+
+
+    private SearchContract.View mView;
+
+    private ArtistRepository mArtistRepository;
+
+    private CompositeDisposable mCompositeDisposable;
+
+
+    @Inject
+    public SearchPresenter(ArtistRepository artistRepository) {
+        this.mArtistRepository = artistRepository;
+        this.mCompositeDisposable = new CompositeDisposable();
+    }
+
+
+    @Override
+    public void startSearch(String artistName) {
+        Disposable disposable = mArtistRepository.searchArtist(artistName)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(artists -> mView.startListActivity(artistName),
+                        throwable -> mView.showError(throwable.getMessage()));
+        mCompositeDisposable.add(disposable);
+    }
+
+    @Override
+    public void setView(SearchContract.View view) {
+        mView = view;
+    }
+
+    @Override
+    public void dropView() {
+        mView = null;
+        mCompositeDisposable.clear();
+        mCompositeDisposable = null;
+    }
+}
