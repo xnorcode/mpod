de86596e9eefd9fc3213349893b4cbd16e06cbd6
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-07-16 06:10:07 +0300

    refactor: testing list screen

--- app/src/androidTest/java/com/mpod/ui/list/ListScreenTest.java
+++ app/src/androidTest/java/com/mpod/ui/list/ListScreenTest.java
@@ -2,10 +2,6 @@ package com.mpod.ui.list;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.assertion.ViewAssertions;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -14,60 +10,108 @@ import com.mpod.MpodApplication;
 import com.mpod.R;
 import com.mpod.utils.AppConstants;
 
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by xnorcode on 15/07/2018.
  */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ListScreenTest {
 
 
     @Rule
     public ActivityTestRule<ListActivity> mListActivityTestRule =
-            new ActivityTestRule<ListActivity>(ListActivity.class) {
+            new ActivityTestRule<>(ListActivity.class);
 
-                @Override
-                protected void beforeActivityLaunched() {
-                    super.beforeActivityLaunched();
-                    // clear database before each activity launch
-                    ((MpodApplication) InstrumentationRegistry.getTargetContext()
-                            .getApplicationContext()).getArtistRepository().deleteAllArtists();
-                }
-
-                @Override
-                protected Intent getActivityIntent() {
-                    // set intent with artist to search coming from search screen
-                    Context targetContext = InstrumentationRegistry.getInstrumentation()
-                            .getTargetContext();
-                    Intent intent = new Intent(targetContext, ListActivity.class);
-                    intent.putExtra(AppConstants.ARTIST_NAME, "Cher");
-                    return intent;
-                }
-            };
+    @Before
+    public void clearDb() {
+        ((MpodApplication) mListActivityTestRule.getActivity().getApplicationContext())
+                .getArtistRepository().deleteAllArtists();
+    }
 
     @Test
-    public void searchArtist_showArtists() {
+    public void testB_searchArtist_showArtists() {
+
+        // launching activity with input data
+        Context targetContext = getInstrumentation().getTargetContext();
+        Intent intent = new Intent(targetContext, ListActivity.class);
+        intent.putExtra(AppConstants.ARTIST_NAME, "name");
+        mListActivityTestRule.launchActivity(intent);
 
         // verify list is enabled
-        Espresso.onView(ViewMatchers.withId(R.id.list_recyclerView))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.list_recyclerView)).check(matches(isDisplayed()));
 
         // verify list items are shown
-        Espresso.onView(ViewMatchers.withText("name"))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withText("name")).check(matches(isDisplayed()));
+
+        // close activity
+        mListActivityTestRule.finishActivity();
     }
 
     @Test
-    public void searchArtist_showError() {
+    public void testA_searchArtist_showError() {
 
+        // launching activity with input data that produce an error
+        Context targetContext = getInstrumentation().getTargetContext();
+        Intent intent = new Intent(targetContext, ListActivity.class);
+        intent.putExtra(AppConstants.ARTIST_NAME, "");
+        mListActivityTestRule.launchActivity(intent);
+
+        // verify list is enabled
+        onView(withId(R.id.list_recyclerView)).check(matches(isDisplayed()));
+
+        // verify toast message is displayed
+        onView(withText("Could not download artists..."))
+                .inRoot(withDecorView(not(mListActivityTestRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
+
+        // close activity
+        mListActivityTestRule.finishActivity();
     }
 
     @Test
-    public void searchArtist_showDetails() {
+    public void testC_searchArtist_showDetails() {
 
+        // launching activity with input data
+        Context targetContext = getInstrumentation().getTargetContext();
+        Intent intent = new Intent(targetContext, ListActivity.class);
+        intent.putExtra(AppConstants.ARTIST_NAME, "name");
+        mListActivityTestRule.launchActivity(intent);
+
+        // verify list is enabled
+        onView(withId(R.id.list_recyclerView)).check(matches(isDisplayed()));
+
+        // click on item that has a children item with text "name"
+        onView(withChild(withText("name"))).perform(click());
+
+        // verify details activity intent has been sent with extra data
+        intended(allOf(
+                toPackage("com.mpod.ui.details"),
+                hasExtraWithKey(AppConstants.ARTIST_MBID)));
+
+        // close activity
+        mListActivityTestRule.finishActivity();
     }
 }
